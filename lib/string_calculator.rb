04861a0a3f3cc55929806d194e33801dc666e220
Asad <asadfazlani99@gmail.com>
2025-06-25 13:27:01 +0530

    Handle any amount of comma-separated numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,6 +2,6 @@
 class StringCalculator
   def self.add(input)
     return 0 if input.empty?
-    return input.to_i unless input.include?(",")
-    input.split(',').map(&:to_i).sum  end
+    input.split(',').map(&:to_i).sum
+  end
 end
