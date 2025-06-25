14de6de38359fb45ff898759a1e51ed189c47379
Asad <asadfazlani99@gmail.com>
2025-06-25 14:04:38 +0530

    Refactor the method

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,17 +1,11 @@
 # lib/string_calculator.rb
 class StringCalculator
   def self.add(input)
-    return 0 if input.empty?
-    delimiter = /,|\n/
-      if input.start_with?("//")
-        header, input = input.split("\n", 2)
-        delimiter = Regexp.escape(header[2])
-      end
-    numbers = input.split(/#{delimiter}/).map(&:to_i)
-      negatives = numbers.select { |n| n < 0 }
-      unless negatives.empty?
-        raise "Negative numbers not allowed: #{negatives.join(', ')}"
-      end
+    numbers = input.scan(/-?\d+/).map(&:to_i)
+    negatives = numbers.select { |n| n < 0 }
+    unless negatives.empty?
+      raise ArgumentError, "Negative numbers not allowed: #{negatives.join(', ')}"
+    end
     numbers.sum
   end
 end
